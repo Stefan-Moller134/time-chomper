6a3b05e19d9506c1dc7a454a06d07e6e5490ad6e
Stefan Moller <158828134+Stefan-Moller134@users.noreply.github.com>
2024-05-20 18:56:40 +0800

    time-chomper V1.1
    update:
    * added local storage functionality
    
     * refactored and improved js and html
    
    Co-authored-by: Xavier <xavier.tnc@gmail.com>

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
+class Entry {
+    static id = 0;
+    constructor(title, time) {
+        this.id = Entry.id++;
+        this.title = title;
+        this.time = time;
+    }
+}
+
+function findEntry(id) {
+    return entries.find(entry => entry.id === id);
+}
+
+function deleteEntry(id) {
+    const entry = findEntry(id);
+    const index = entries.indexOf(entry);
+    entries.splice(index, 1);
+}
+
+
 const textInput = document.getElementById('main-input');
 const entryItem = document.querySelector('.entry-item');
-let deleteBtn = document.querySelector('.delete-btn');
+const deleteBtn = document.querySelector('.delete-btn');
 const stopStartBtn= document.getElementById('startStop');
 const entriesList = document.getElementById('entries-container');
-let display = document.getElementById('display');
-let hungryText = document.getElementById('hungry');
+const chompyIsHungry = document.getElementById('chompyIsHungry');
+const timeDisplay = document.getElementById('timeDisplay');
+const entryTemplate = document.getElementById('entryTemplate');
+const entries = [];
 
 // stop watch
 let seconds = 0, minutes = 0, hours = 0;
@@ -15,11 +37,11 @@ let stopWatchStatus = 'stopped';
 function stopWatch() {
     seconds++;
 
-    if (seconds / 60 === 1) {
+    if (seconds / 60 >= 1) { 
         seconds = 0;
         minutes++;
 
-        if (minutes / 60 === 1) {
+        if (minutes / 60 >= 1) {
             minutes = 0;
             hours++;
         }
@@ -44,7 +66,7 @@ function stopWatch() {
     }
 
 
-    display.innerHTML = displayHours + ':' + displayMinutes + ':' + displaySeconds;
+    timeDisplay.innerHTML = displayHours + ':' + displayMinutes + ':' + displaySeconds;
 }
 
 function startStop() {
@@ -66,35 +88,100 @@ function startStop() {
 
 // Adds entry to the list
 function addEntry() {
-    const text = textInput.value;
+   
+    if (stopWatchStatus === 'started') return;
+
+    if (stopWatchStatus === 'stopped') {
+
+        const entry = new Entry(textInput.value, timeDisplay.innerHTML);
+
+        if (entry.title === '') return alert('Please enter a byte!');
+
+        const clone = entryTemplate.content.cloneNode(true);
     
-    if (stopWatchStatus === 'started') {
-        return;;
-    } 
-    if (text === '') {
-        alert('Please enter a value');
-    } else if (stopWatchStatus === 'stopped') {
-        entriesList.innerHTML += `
-        <div class="entry-row"> 
-            <div class="entry-text"><p>${text}</p></div>
-            <div class="action-container">
-                <div>${display.innerHTML}</div>
-                <button class="delete-btn">Delete</button>
-            </div>
-        </div>
-    `;
-    hungryText.innerHTML = '';
-    textInput.value = '';
-    display.innerHTML = '00:00:00';
+        const title = clone.querySelector('.entry-text');
+        const time = clone.querySelector('.entry-time');
+        const deleteBtn = clone.querySelector('.delete-btn');
+        
+        title.textContent = entry.title;
+        time.textContent = entry.time;
+        deleteBtn.value = entry.id;
+
+        entriesList.appendChild(clone);
+
+        textInput.value = '';
+        timeDisplay.innerHTML = '00:00:00';
+        chompyIsHungry.style.display = 'none';
+
+        entries.push(entry);
+
+        console.log(entries);
     }
 }
 
+
 stopStartBtn.addEventListener('click', addEntry);
+
 
 // Deletes entry from the list
 entriesList.addEventListener('click', function(event) {
-    if (event.target.classList.contains('delete-btn')) {
-        event.target.parentElement.parentElement.remove();
+    const deleteButton = event.target;
+    if (deleteButton.classList.contains('delete-btn')) {
+        deleteButton.parentElement.parentElement.remove();
+        if (entriesList.children.length === 0) {
+            setTimeout(function() { chompyIsHungry.style.display = 'block'; });
+        }
+        const entityId = parseInt(deleteButton.value);
+        deleteEntry(entityId);
+        console.log(entries);
     };
 });
+
+
+// Save entries to local storage
+window.addEventListener('beforeunload', function() {
+    localStorage.setItem('entries', JSON.stringify(entries));
+});
+
+
+// load entries from local storage
+window.addEventListener('load', function() {
+    const storedEntries = JSON.parse(localStorage.getItem('entries'));
+    if (storedEntries) {
+        storedEntries.forEach(entry => {
+            const clone = entryTemplate.content.cloneNode(true);
+    
+            const elTitle = clone.querySelector('.entry-text');
+            const elTime = clone.querySelector('.entry-time');
+            const elDeleteBtn = clone.querySelector('.delete-btn');
+            
+            elTitle.textContent = entry.title;
+            elTime.textContent = entry.time;
+            elDeleteBtn.value = entry.id;
+
+            entriesList.appendChild(clone);
+
+            entries.push(entry);
+
+            console.log(entries);
+        });
+
+        if (entries.length > 0) chompyIsHungry.style.display = 'none';
+    } // if storedEntries
+}); // window load
+
+
+// // clear local storage
+// window.addEventListener('storage', function() {
+//     localStorage.clear();
+// });
+
+// // clear all entries
+// const clearAllBtn = document.getElementById('clearAll');
+// clearAllBtn.addEventListener('click', function() {
+//      entriesList.innerHTML = '';
+//     entries.length = 0;
+//     localStorage.clear();
+//     chompyIsHungry.style.display = 'block';
+// });
 
